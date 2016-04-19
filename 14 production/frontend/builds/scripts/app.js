e96dbd774d7ccda3248a3ca174725db1ab582209
jclaw <jclaw3456@gmail.com>
2016-04-19 16:22:53 -0400

    orb centered and pulsing

--- 14 production/frontend/builds/scripts/app.js
+++ 14 production/frontend/builds/scripts/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+// $('.orb-inner').addClass('infinite').animateCss('pulse');
