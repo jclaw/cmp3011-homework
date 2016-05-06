927fa90bd4e9dfb19322c17043ff8fd952f2564a
jclaw <jclaw3456@gmail.com>
2016-05-06 18:27:46 -0400

    removed extra folders

--- folder2/player.js
+++
@@ -1,9 +0,0 @@
-angular.module('earApp')
-.controller('PlayerCtrl', function($scope) {
-   $scope.first = "Info";
-    $scope.customers=[
-        {name:'jerry',city:'chicago'},
-        {name:'tom',city:'houston'},
-        {name:'enslo',city:'taipei'}
-    ];
-});
